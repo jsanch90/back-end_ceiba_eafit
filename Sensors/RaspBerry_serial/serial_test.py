4fda92033ff94f40eddaff95049dd11b8dce2c62
jsanch90 <jsanch90@eafit.edu.co>
2019-03-04 18:13:24 -0500

    json_arduino BOSCH

---
+++ Sensors/RaspBerry_serial/serial_test.py
@@ -0,0 +1,9 @@
+import serial
+port = "/dev/ttyACM0"
+s1 = serial.Serial(port,9600)
+s1.flushInput()
+
+while True:
+    if s1.inWaiting()>0:
+        inputValue = s1.readline()
+        print(inputValue)
