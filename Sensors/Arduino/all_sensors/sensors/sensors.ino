c5cc5f2aea8436277063b359c98766ac5cf8a49e
jsanch90 <jsanch90@eafit.edu.co>
2019-03-04 18:16:21 -0500

    codigo limpio del BOSHC con temporizador para enviar las variables

--- Sensors/Arduino/all_sensors/sensors/sensors.ino
+++ Sensors/Arduino/all_sensors/sensors/sensors.ino
@@ -1,6 +1,10 @@
 #include <CurieBLE.h>
 //Enable debug to be able to print in arduino IDE
 bool debug = true;
+long h_priority_time = 5000;
+long l_priority_time = 15000;
+long last_time_h = 0;
+long last_time_l = 0;
 
 unsigned char data2[20] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
@@ -47,12 +51,8 @@ void loop()
         {
             delay(20);
             BLEService HighPrioDataService = peripheral.service("c2967210-7ba4-11e4-82f8-0800200c9a66");
-            //exploreService(HighPrioDataService);
-
             BLECharacteristic HighPrioChar = HighPrioDataService.characteristic("c2967211-7ba4-11e4-82f8-0800200c9a66");
             BLECharacteristic LowPrioChar = HighPrioDataService.characteristic("c2967212-7ba4-11e4-82f8-0800200c9a66");
-            //exploreHighCharacteristic(HighPrioChar);
-            //exploreLowCharacteristic(LowPrioChar);
 
             if (HighPrioChar.canRead())
             {
@@ -61,7 +61,10 @@ void loop()
 
                 if (HighPrioChar.valueLength() > 0)
                 {
+                  if((millis() - last_time_h) >= h_priority_time){
                     printHighPrioData(HighPrioChar.value(), HighPrioChar.valueLength());
+                    last_time_h = millis();
+                  }
                 }
             }
 
@@ -70,7 +73,10 @@ void loop()
                 LowPrioChar.read();
                 if (LowPrioChar.valueLength() > 0)
                 {
+                  if((millis() - last_time_l) >= l_priority_time){
                     printLowPrioData(LowPrioChar.value(), LowPrioChar.valueLength());
+                    last_time_l = millis();
+                  }
                 }
             }
         }
@@ -81,16 +87,6 @@ void printHighPrioData(const unsigned char data[], int length)
 {
     if (debug)
     {
-        //Serial.print(", value: ");
-        for (int i = 0; i < length; i++)
-        {
-            unsigned char b = data[i];
-
-            //Serial.print(b);
-            //Serial.print(",");
-        }
-        //Serial.println();
-
         long accX = (byte(data[1]) << 8) | byte(data[0]);
         long accY = (byte(data[3]) << 8) | byte(data[2]);
         long accZ = (byte(data[5]) << 8) | byte(data[4]);
@@ -123,33 +119,26 @@ void printHighPrioData(const unsigned char data[], int length)
             gyroZ = gyroZ - 65535;
         }
 
-        Serial.print("Accelerometer X --> ");
-        Serial.println(accX);
-        Serial.print("Accelerometer Y --> ");
-        Serial.println(accY);
-        Serial.print("Accelerometer Z --> ");
-        Serial.println(accZ);
-        Serial.print("Gyro X --> ");
-        Serial.println(gyroX);
-        Serial.print("Gyro Y --> ");
-        Serial.println(gyroY);
-        Serial.print("Gyro Z --> ");
-        Serial.println(gyroZ);
+        Serial.print("XDK_AX;");
+        Serial.print("XDK_AY;");
+        Serial.print("XDK_AZ;");
+        Serial.print("XDK_GX;");
+        Serial.print("XDK_GY;");
+        Serial.print("XDK_GZ,");
+        Serial.print(accX);
+        Serial.print(";");
+        Serial.print(accY);
+        Serial.print(";");
+        Serial.print(accZ);
+        Serial.print(";");
+        Serial.print(gyroX);
+        Serial.print(";");
+        Serial.print(gyroY);
+        Serial.print(";");
+        Serial.print(gyroZ);
+        Serial.println();
     }
-    //if (enableXbee){
-    //Serial1.println("High");
 
-    //Serial1.print(length-1);
-    //data[19]=1;
-    //Serial1.write(data, length);
-    //Serial1.print('H');
-    /*
-      for(int i = 0; i < length; i++) {
-        Serial1.print(data[i]);
-        }
-      Serial1.print('\n');
-    //}
-    */
 }
 
 void printLowPrioData(const unsigned char data[], int length)
@@ -157,68 +146,50 @@ void printLowPrioData(const unsigned char data[], int length)
     if (debug)
     {
 
-        //Serial.print(", value: ");
-
         if (int(data[0]) == 1)
         {
-            //Serial.print("Message 1: ");
-            for (int i = 0; i < length; i++)
-            {
-                unsigned char b = data[i];
-                //Serial.print(b);
-                //Serial.print(",");
-            }
-            //Serial.println();
             long lightSensor = (byte(data[4]) << 24) | (byte(data[3]) << 16) | (byte(data[2]) << 8) | byte(data[1]);
             long noise = byte(data[5]);
             long airPress = (byte(data[9]) << 24) | (byte(data[8]) << 16) | (byte(data[7]) << 8) | byte(data[6]);
             long temp = (byte(data[13]) << 24) | (byte(data[12]) << 16) | (byte(data[11]) << 8) | byte(data[10]);
             long humidity = (byte(data[17]) << 24) | (byte(data[16]) << 16) | (byte(data[15]) << 8) | byte(data[14]);
 
-            Serial.print("Light Sensor --> ");
-            Serial.println(lightSensor);
-            Serial.print("Noise Sensor --> ");
-            Serial.println(noise);
-            Serial.print("Atmospheric Pressure --> ");
-            Serial.println(airPress);
-            Serial.print("Temperature --> ");
-            Serial.println(temp);
-            Serial.print("Humidity --> ");
-            Serial.println(humidity);
+            Serial.print("XDK_L;");
+            Serial.print("XDK_N;");
+            Serial.print("XDK_A;");
+            Serial.print("XDK_T;");
+            Serial.print("XDK_H,");
+            Serial.print(lightSensor);
+            Serial.print(";");
+            Serial.print(noise);
+            Serial.print(";");
+            Serial.print(airPress);
+            Serial.print(";");
+            Serial.print(temp);
+            Serial.print(";");
+            Serial.print(humidity);
+            Serial.println();
         }
         else
         {
-            //Serial.print("Message 2: ");
-            for (int i = 0; i < length; i++)
-            {
-                unsigned char b = data[i];
-                //Serial.print(b);
-                //Serial.print(",");
-            }
-            //Serial.println();
+
             long magnX = (byte(data[2]) << 8) | byte(data[1]);
             long magnY = (byte(data[4]) << 8) | byte(data[3]);
             long magnZ = (byte(data[6]) << 8) | byte(data[5]);
             long magnRes = (byte(data[8]) << 8) | byte(data[7]);
 
-            Serial.print("Magnetometer X --> ");
-            Serial.println(magnX);
-            Serial.print("Magnetometer Y --> ");
-            Serial.println(magnY);
-            Serial.print("Magnetometer Z --> ");
-            Serial.println(magnZ);
-            Serial.print("Magnetometer Z --> ");
-            Serial.println(magnZ);
-            Serial.print("Magnetometer Resistance: ");
-            Serial.println(magnRes);
+            Serial.print("XDK_MX;");
+            Serial.print("XDK_MY;");
+            Serial.print("XDK_MZ;");
+            Serial.print("XDK_MRes,");
+            Serial.print(magnX);
+            Serial.print(";");
+            Serial.print(magnY);
+            Serial.print(";");
+            Serial.print(magnZ);
+            Serial.print(";");
+            Serial.print(magnRes);
+            Serial.println();
         }
     }
-    //if (enableXbee){
-    //Serial1.println("Low");
-    //  Serial1.write('\n');
-    //data[19]= 2;
-    //Serial1.write(data, length);
-    //Serial1.print('L');
-
-    //}
 }
