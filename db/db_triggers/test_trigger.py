b9d801e6b6307352e840cce867034d1ad5d6cd67
jsanch90 <jsanch90@eafit.edu.co>
2019-03-11 17:44:22 -0500

    MongoTriggers, activacion cuando hay un update

--- db/db_triggers/test_trigger.py
+++ db/db_triggers/test_trigger.py
@@ -1,19 +1,17 @@
 from mongotriggers import MongoTrigger
 from pymongo import MongoClient
+from time import sleep
 
 def notify_manager(op_document):
-    print ('wake up! someone is adding me money')
+    print ('Document updated!')
 
 client = MongoClient(host='localhost', port=27017)
 triggers = MongoTrigger(client)
 
-# listens to update/insert/delete, any of these will trigger the callback
-triggers.register_op_trigger(notify_manager, 'my_account', 'my_transactions')
+# listens to update
+triggers.register_update_trigger(notify_manager, 'my_account', 'my_transactions')
 triggers.tail_oplog()
 
-# make an operation to simulate interaction
-#client['my_account']['my_transactions'].insert_one({"balance": 1000})
-#triggers.stop_tail()
-
 while True:
-    y= 1
+    print("inside the bucle")
+    sleep(20)
