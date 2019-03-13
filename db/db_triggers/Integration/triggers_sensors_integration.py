c75cb40805ed78e1e950807bb3d5d6a0d39cd816
jsanch90 <jsanch90@eafit.edu.co>
2019-03-13 10:07:58 -0500

    integrating triggers with sensors

--- db/db_triggers/Integration/triggers_sensors_integration.py
+++ db/db_triggers/Integration/triggers_sensors_integration.py
@@ -1,12 +1,10 @@
-"""
-mongodb version = 2.4.1
-pymongo version = 3.2
-"""
-
 from mongotriggers import MongoTrigger
 from pymongo import MongoClient
 import RPi.GPIO as GPIO
+import serial
+import json
 
+#Relays config
 relayPin1 = 38 #relay 1
 relayPin2 = 40 #relay 2
 
@@ -14,11 +12,31 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(relayPin1, GPIO.OUT)
 GPIO.setup(relayPin2, GPIO.OUT)
 
+#DB config
 client = MongoClient(host='localhost', port=27017)
 db = client.test_relays
 triggers = MongoTrigger(client)
 
-#callback function, this function is called when some register is updated in the database
+#Arduino_serial config
+port = "/dev/ttyACM0" # USB port where Arduino is connected
+s1 = serial.Serial(port,9600) # Start the serial port
+s1.flushInput()
+
+def make_json(line):
+    parts = line.split(',')
+    var_names = parts[0].split(';')
+    values = parts[1].strip('\r\n').split(';')
+    json_body=''
+    #print(var_names)
+    #print(values)
+    for i in range(len(var_names)):
+        if i == len(var_names):
+            json_body+='{0}:{1}\n'.format(var_names[i],values[i])
+        json_body+='{0}:{1},\n'.format(var_names[i],values[i])
+    last_comma_index = json_body.rfind(',')
+    json_body = json_body[:last_comma_index]+json_body[last_comma_index+1:]
+    return '{\n'+'{0}'.format(json_body)+'}'
+
 def set_relays_state(op_document=None):
     state_rel_1 = db.relays.find({"relay" : 1})[0]['state']
     state_rel_2 = db.relays.find({"relay" : 2})[0]['state']
@@ -39,10 +57,15 @@ def set_relays_state(op_document=None):
     
     print('Document updated!')
 
-#starting 
-set_relays_state()
+def send_register_to_db(json_record):
+    body = json.loads(json_record)
+    print(body)
 
-# listens to update
 triggers.register_update_trigger(set_relays_state, 'test_relays', 'relays')
 triggers.tail_oplog()
 
+while True:
+    if s1.inWaiting()>0:
+        line = s1.readline()
+        if len(line) > 1:
+            send_register_to_db(make_json(line))
